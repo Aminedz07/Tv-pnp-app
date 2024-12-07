de550a940c9c4c5fc574f6a8be14f7dfb9e808f6
Aminedz07 <103056572+Aminedz07@users.noreply.github.com>
2024-12-07 23:20:21 +0000

    Ajout de la navigation et mise à jour du contenu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+# Title of the app
+st.title("My Streamlit App")
+st.write("Hello world ")
+    
+# Sidebar for navigation
+st.sidebar.header("Navigation")
+page = st.sidebar.selectbox("Select a page", ["Home", "About", "Contact"])
+
+if page == "Home":
+    st.subheader("Welcome to My App")
+    st.write("This is the home page.")
+elif page == "About":
+    st.subheader("About")
+    st.write("This app is built using Streamlit.")
+elif page == "Contact":
+    st.subheader("Contact")
+    st.write("You can reach us at contact@example.com")
