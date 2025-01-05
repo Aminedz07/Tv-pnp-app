2d67355d2637cfd791d75bd5b874c3bb17ca5a6c
Aminedz07 <103056572+Aminedz07@users.noreply.github.com>
2025-01-05 16:34:08 +0000

    pas de messagei

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,7 @@ if uploaded_file is not None:
         if st.button("Débruiter"):
             # Ajout du bruit à l'image
             
-            noisy_image=operateur(image).noise(sigma)
+            noisy_image=operateur(image).noise(noise_level)
             
     
             # Calcul du PSNR avant le débruitage
